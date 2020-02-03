933f5749d74ef0c551a3c992660867ec96f594f2
maxerokhovets <maxerokhovets@gmail.com>
2020-02-03 23:10:57 +0300

    NPE in AzimuthToSideOfTheWorldConverter class fixed

--- app/src/main/java/com/nucldev/simpleweatherlocator/ui/main/utils/AzimuthToSideOfTheWorldConverter.java
+++ app/src/main/java/com/nucldev/simpleweatherlocator/ui/main/utils/AzimuthToSideOfTheWorldConverter.java
@@ -8,40 +8,40 @@ public class AzimuthToSideOfTheWorldConverter {
     }
 
     public String getSideOfTheWorld(){
-        SidesOfTheWorld sidesOfTheWorld = null;
-        if(this.mAzimuth>0 & this.mAzimuth<11.25)
+        SidesOfTheWorld sidesOfTheWorld = SidesOfTheWorld.Undefined;
+        if(this.mAzimuth>=0 & this.mAzimuth<11.25)
             sidesOfTheWorld = SidesOfTheWorld.N;
-        if(this.mAzimuth>348.75&this.mAzimuth<360)
+        if(this.mAzimuth>=348.75&this.mAzimuth<=360)
             sidesOfTheWorld = SidesOfTheWorld.N;
-        if(this.mAzimuth>11.25&this.mAzimuth<33.75)
+        if(this.mAzimuth>=11.25&this.mAzimuth<33.75)
             sidesOfTheWorld = SidesOfTheWorld.NNE;
-        if(this.mAzimuth>33.75&this.mAzimuth<56.25)
+        if(this.mAzimuth>=33.75&this.mAzimuth<56.25)
             sidesOfTheWorld = SidesOfTheWorld.NE;
-        if(this.mAzimuth>56.25&this.mAzimuth<78.75)
+        if(this.mAzimuth>=56.25&this.mAzimuth<78.75)
             sidesOfTheWorld = SidesOfTheWorld.ENE;
-        if (this.mAzimuth>78.75&this.mAzimuth<101.25)
+        if (this.mAzimuth>=78.75&this.mAzimuth<101.25)
             sidesOfTheWorld = SidesOfTheWorld.E;
-        if (this.mAzimuth>101.25&this.mAzimuth<123.75)
+        if (this.mAzimuth>=101.25&this.mAzimuth<123.75)
             sidesOfTheWorld = SidesOfTheWorld.ESE;
-        if (this.mAzimuth>123.75&this.mAzimuth<146.25)
+        if (this.mAzimuth>=123.75&this.mAzimuth<146.25)
             sidesOfTheWorld = SidesOfTheWorld.SE;
-        if (this.mAzimuth>146.25&this.mAzimuth<168.75)
+        if (this.mAzimuth>=146.25&this.mAzimuth<168.75)
             sidesOfTheWorld = SidesOfTheWorld.SSE;
-        if (this.mAzimuth>168.75&this.mAzimuth<191.25)
+        if (this.mAzimuth>=168.75&this.mAzimuth<191.25)
             sidesOfTheWorld = SidesOfTheWorld.S;
-        if (this.mAzimuth>191.25&this.mAzimuth<213.75)
+        if (this.mAzimuth>=191.25&this.mAzimuth<213.75)
             sidesOfTheWorld = SidesOfTheWorld.SSW;
-        if (this.mAzimuth>213.75&this.mAzimuth<236.25)
+        if (this.mAzimuth>=213.75&this.mAzimuth<236.25)
             sidesOfTheWorld = SidesOfTheWorld.SW;
-        if (this.mAzimuth>236.25&this.mAzimuth<258.75)
+        if (this.mAzimuth>=236.25&this.mAzimuth<258.75)
             sidesOfTheWorld = SidesOfTheWorld.WSW;
-        if (this.mAzimuth>258.75&this.mAzimuth<281.25)
+        if (this.mAzimuth>=258.75&this.mAzimuth<281.25)
             sidesOfTheWorld = SidesOfTheWorld.W;
-        if (this.mAzimuth>281.5&this.mAzimuth<303.75)
+        if (this.mAzimuth>=281.25&this.mAzimuth<303.75)
             sidesOfTheWorld = SidesOfTheWorld.WNW;
-        if (this.mAzimuth>303.75&this.mAzimuth<326.25)
+        if (this.mAzimuth>=303.75&this.mAzimuth<326.25)
             sidesOfTheWorld = SidesOfTheWorld.NW;
-        if (this.mAzimuth>326.25&this.mAzimuth<348.75)
+        if (this.mAzimuth>=326.25&this.mAzimuth<348.75)
             sidesOfTheWorld = SidesOfTheWorld.NNW;
         return    sidesOfTheWorld.toString();
     }
--- app/src/main/java/com/nucldev/simpleweatherlocator/ui/main/utils/SidesOfTheWorld.java
+++ app/src/main/java/com/nucldev/simpleweatherlocator/ui/main/utils/SidesOfTheWorld.java
@@ -1,5 +1,5 @@
 package com.nucldev.simpleweatherlocator.ui.main.utils;
 
 public enum SidesOfTheWorld {
-    N, NNE, NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW
+    N, NNE, NE, ENE, E, ESE, SE, SSE, S, SSW, SW, WSW, W, WNW, NW, NNW, Undefined
 }
