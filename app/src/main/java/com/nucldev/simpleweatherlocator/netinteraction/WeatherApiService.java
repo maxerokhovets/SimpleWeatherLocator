fedfcd41ba3a8f5c268c242604993761154d9cd7
maxerokhovets <maxerokhovets@gmail.com>
2020-02-13 18:11:20 +0300

    http changed on https in base url

--- app/src/main/java/com/nucldev/simpleweatherlocator/netinteraction/WeatherApiService.java
+++ app/src/main/java/com/nucldev/simpleweatherlocator/netinteraction/WeatherApiService.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WeatherApiService {
     private static WeatherApiService mInstance;
-    private static final String BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
     private Retrofit mRetrofit;
 
     private WeatherApiService() {
